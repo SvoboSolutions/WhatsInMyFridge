f0c419b092ca935c54551971ea09df6e81fe3b8a
Kai Svoboda <kaichristiansvoboda@bundeswehr.org>
2026-08-28 12:29:46 +0200

    Fix iOS-Crash: FirebaseApp.configure() beim App-Start ergänzen
    Firebase Auth crasht mit einem fatalError, wenn FirebaseApp.configure()
    nicht vor der ersten Nutzung aufgerufen wurde - fehlte bisher komplett
    in der SwiftUI-App.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,16 @@
 import SwiftUI
 import GoogleSignIn
+import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+
     func application(
         _ app: UIApplication,
         open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]
